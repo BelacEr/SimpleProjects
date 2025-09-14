e350645bff13764f9228f7eed68ba5d677644aeb
BelacEr <caleblopezmorgado2007@gmail.com>
2025-09-13 18:13:26 -0600

    books-to-read program

---
+++ books_to_read/books_to_read_modules.py
@@ -0,0 +1,105 @@
+import json
+
+import sys
+
+import os
+
+# Global variables.
+filename = "books.json"
+
+
+
+def add_book():
+    """Function to add the book name and the author in the JSON file."""
+    try:
+        # Ask for the book and author name with try-except for built-in exceptions.
+        book = input("\nEnter the book name: ").strip()
+        author = input("Enter the author name: ").strip()
+    except (KeyboardInterrupt, EOFError):
+        print("Goodbye!")
+        return # Exit early
+
+    new_data = {book: author}
+
+    try:
+        if os.path.exists(filename):
+            with open(filename, "r") as file:
+                try:
+                    data = json.load(file)
+                except json.JSONDecodeError:
+                    data = {}   # File is emtpy or corrupted, reset to emtpy dict.
+        else:
+            data = {}
+    
+        # Update dictionary with new data.
+        data.update(new_data)
+
+        # Write back to file (overwrite with updated data)
+        with open(filename, "w") as file:
+            json.dump(data, file, indent=4)
+
+        print(f"Book '{book}' by {author} succesfully added to {filename}.")
+    except IOError as e:
+        print(f"Error writing to fle '{filename}': {e}")
+
+
+def read_book():
+    try:
+        with open(filename, "r") as file:
+            loaded_data = json.load(file)
+            print(f"JSON data successfully loaded from {filename}.\n")
+            
+            print("Books in library:")
+            for book, author in loaded_data.items():
+                print(f"- {book} by {author}")
+
+    except FileNotFoundError:
+        print(f"The file '{filename}' was not found.")
+    except IOError as e:
+        print(f"Error loading to file {filename}: {e}")
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON from '{filename}': {e}")
+
+
+
+
+def delete_book():
+    print('Delete book')
+
+
+
+def show_menu():
+    print("""
+==== BOOKS-TO-READ ====
+1. Add new book
+2. See books
+3. Delete books
+4. Exit
+""")
+
+def main():
+    """Main functin of the program."""
+    while True:
+        show_menu() # Show the menu of the program.
+        try:
+            # Ask for a number that will be used in the menu with try-except for built-in exceptions.
+            choice = int(input("Choose the option: "))
+
+            # Temporary menu selection. Switch from if-elif-else to dictionary later.
+            if choice == 1:
+                add_book()
+            elif choice == 2:
+                read_book()
+            elif choice == 3:
+                delete_book()
+            elif choice == 4:
+                sys.exit()
+            else:
+                print("\nPlease make sure to enter a valid number.")
+
+        except ValueError:
+            print("\nPlease make sure to enter a valid number.")
+        except (KeyboardInterrupt ,EOFError):
+            print("\nGoodbye!")
+            sys.exit()
+
---
+++ books_to_read/main.py
@@ -0,0 +1,5 @@
+from books_to_read_modules import main
+
+# Run the program.
+if __name__ == "__main__":
+    main()
