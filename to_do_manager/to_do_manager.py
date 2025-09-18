61772f44535fcf02bf29d5bcf27b55cf977c2f6e
BelacEr <caleblopezmorgado2007@gmail.com>
2025-09-18 14:19:00 -0600

    The delete function has been added.

--- to_do_manager/to_do_manager.py
+++ to_do_manager/to_do_manager.py
@@ -5,12 +5,13 @@ import sys
 import os
 
 
-def show_menu():    # The ability to change status and delete tasks will be added.
+def show_menu():    # The ability to change status will be added.
     print("""
 ===== TO-DO LIST MENU =====
 1. Add Task
 2. View Task
-3. Exit
+3. Delete Task
+4. Exit
 """)
 
 
@@ -53,10 +54,10 @@ def save_json_data(data: dict) -> bool:
         print(f"Error writing to file 'tasks.json': {e}.")
         return False
 
-            
+
 def add_task():
     """Add a task to the JSON file that includes a try-except statement for built-in exceptions."""
-    task = get_non_emtpy_input("Enter the task: ")
+    task = get_non_emtpy_input("\nEnter the task: ")
     status = "Not completed"
 
     new_data = {task: status}
@@ -83,7 +84,30 @@ def read_tasks():
         for task, status in data.items():
             print(f"- {task}:  {status}")
     else:
-        print("There are no tasks.")
+        print("\nThere are no tasks.")
+
+
+def delete_task():
+    """Delete a specific task by searching for its name."""
+    tasks = read_tasks()
+    data = load_json_data()
+
+    if data:
+        # If there are tasks, you will be prompted to select the one you want to delete.
+        task = get_non_emtpy_input("\nEnter the name of the task you want to delete: ")
+    else:
+        # If there are no tasks, you will be redirected back to the menu.
+        return
+
+    if task in data:
+        # Delete the task by its name.
+        del data[task]
+        print(f"The task '{task}' was deleted.")
+
+         # Update the JSON file with new data.
+        save_json_data(data)
+    else:
+        print(f"The task {task} was not found in tasks.json")
 
 
 def exit_program():
@@ -96,7 +120,8 @@ def main():
     menu_options = {
         1: add_task,
         2: read_tasks,
-        3: exit_program,
+        3: delete_task,
+        4: exit_program,
     }
     while True:
         show_menu() # Show the menu of the program.
@@ -109,7 +134,9 @@ def main():
                 selected_function()
             else:
                 print("\nPlease make sure to enter a valid option.")
-                return 
+    
+        except ValueError:
+            print("\nPlease makse sure to enter a valid number.")
 
         except (KeyboardInterrupt, EOFError):
             print("\nThank you for using TO-DO MANAGER by BelacEr!")
