a9bd8b0fc17846671034232e817f5e2306905e1a
BelacEr <caleblopezmorgado2007@gmail.com>
2025-09-18 00:24:16 -0600

    to_do_manager_cli.py

--- to_do_manager/main.py
+++ to_do_manager/main.py
@@ -1,19 +1,5 @@
-import to_do_manager as to_do
+from to_do_manager import main  
 
-while True:
-    to_do.show_menu()
-    choice = input("Enter your choice:")
-
-    if choice == 1:
-        to_do.add_task()
-    elif choice == 2:
-        to_do.view_task()
-    elif choice == 3:
-        to_do.mark_task()
-    elif choice == 4:
-        to_do.delete_task()
-    elif choice == 5:
-        print("Good bye!")
-        break
-    else:
-        print("Invalid choice. Please try again!")
+# Run the program.
+if __name__ == '__main__':
+    main()
